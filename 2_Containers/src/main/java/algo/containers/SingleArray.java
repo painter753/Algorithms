ef18d4d87ea91661cbd71468995426f1901b9ee3
IASmirnov <painter774@gmail.com>
2020-02-16 22:40:06 +0300

    Fix some bugs and do some refactor

--- 2_Containers/src/main/java/algo/containers/SingleArray.java
+++ 2_Containers/src/main/java/algo/containers/SingleArray.java
@@ -1,81 +1,19 @@
 package algo.containers;
 
-public class SingleArray<T> implements IArray<T> {
+public class SingleArray<T> extends AbstractArray<T> {
 
-    // causes problems because array = new T[capaacity] throw error and you should create Object[] and cast to T[]
-    private T[] array;
-    private int size;
-
-    //private Object[] array2;
+    public SingleArray() {
+        this(0);
+    }
 
     @SuppressWarnings("unchecked")
-    public SingleArray() {
-        array = (T[]) new Object[0];
-        size = array.length;
+    private SingleArray(int initCapacity) {
+        super(initCapacity);
     }
 
-    @Override
-    public int size() {
-        return size;
+    protected int getCapacityIncrement() {
+        return 1;
     }
 
-    @Override
-    public void add(T item) {
-        array = resize(array,1);
-        size = array.length;
-        array[size() - 1] = item;
-    }
 
-    @Override
-    public T get(int index) {
-        return array[index];
-    }
-
-    @Override
-    public void clear() {
-        array = (T[]) new Object[0];
-        size = array.length;
-    }
-
-    @Override //todo check method
-    public void add(T item, int index) {
-        if (index < 0 || index > size()) {
-            throw new IndexOutOfBoundsException();
-        }
-
-        int oldArraySize = size();
-        array = resize(array, 1);
-        size = array.length;
-        System.arraycopy(array, index, array, index + 1, oldArraySize - index );
-        array[index] = item;
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")//todo check method
-    public T remove(int index) {
-        if (index < 0 || index > size()) {
-            throw new IndexOutOfBoundsException();
-        }
-
-        T item = get(index);
-        if (index == size() - 1 ) {
-            array[index] = null;
-
-        } else {
-            System.arraycopy(array, index + 1, array, index, size() - index - 1);
-        }
-        size--;
-
-        return item;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        if (array.length == 0) return "no";
-        for (int i = 0; i < size(); i++) {
-            builder.append(array[i].toString()).append(" ");
-        }
-        return builder.toString();
-    }
 }
--- 2_Containers/src/main/java/algo/containers/VectorArray.java
+++ 2_Containers/src/main/java/algo/containers/VectorArray.java
@@ -1,94 +1,26 @@
 package algo.containers;
 
-public class VectorArray<T> implements IArray<T>{
+public class VectorArray<T> extends AbstractArray<T>{
 
-
-    // causes problems because array = new T[capaacity] throw error and you should create Object[] and cast to T[]
-    private T[] array;
     private int vector;
-    private int size;
-
-
-    //private Object[] array2;
-
-    @SuppressWarnings("unchecked")
-    public VectorArray(int vector) {
-        array = (T[]) new Object[0];
-        this.vector = vector;
-        this.size = 0;
-    }
-
 
     public VectorArray() {
-        this(100);
+        this(0, 100);
     }
 
-    @Override
-    public int size() {
-        return size;
+    public VectorArray(int vector) {
+        this(0, vector);
     }
 
-    @Override
-    public void add(T item) {
-        if( size() == array.length)
-            array = resize(array, vector);
-        array[size] = item;
-        size++;
+    @SuppressWarnings("unchecked")
+    private VectorArray(int initCapacity, int vector) {
+        super(initCapacity);
+        this.vector = vector;
     }
 
-    @Override
-    public T get(int index) {
-        return array[index];
-    }
-
-    @Override
-    public void clear() {
-        this.array = (T[]) new Object[0];
-        this.size = 0;
-    }
-
-    @Override
-    public void add(T item, int index) {
-        // add to the end
-        if (index == array.length) {
-            add(item);
-        } else {
-            if (size() == array.length) {
-                //need to resize
-                int oldArraySize = size();
-                array = resize(array, vector);
-                System.arraycopy(array, index, array, index + 1, oldArraySize - index );
-                array[index] = item;
-            } else {
-                //no need for resize
-                System.arraycopy(array, index, array, index + 1, size() - index);
-                array[index] = item;
-            }
-            size++;
-        }
 
 
-    }
-
-    @Override
-    public T remove(int index) {
-        if (index < 0 || index > size()) {
-            throw new IndexOutOfBoundsException("Invalid index:" + index);
-        }
-        T item = array[index];
-        System.arraycopy(array, index + 1, array, index, size() - index - 1);
-        size--;
-
-        return item;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        if (array.length == 0) return "no";
-        for (int i = 0; i < size() ; i++ ) {
-            builder.append(array[i].toString()).append(" ");
-        }
-        return builder.toString();
+    protected int getCapacityIncrement() {
+        return this.vector;
     }
 }
